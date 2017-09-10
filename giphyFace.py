769f46b00fa785dd825b3feb8ce4fc404cc8bac1
Diana Marsala <marsalad@sas.upenn.edu>
2017-09-09 20:50:24 -0400

    added support for animated GIFs

--- giphyFace.py
+++ giphyFace.py
@@ -1,5 +1,7 @@
 from faceswap import *
 import os
+import imageio
+import shutil
 from PIL import Image
 
 # get face landmarks
@@ -9,31 +11,38 @@ mask = get_face_mask(face, landmarks2)
 
 # extract frames
 currFrame = Image.open(gif)
-if not os.path.exists("img"):
-    os.makedirs("img")
+if not os.path.exists("swap"):
+    os.makedirs("swap")
 frameNum = 0
 while currFrame:
-    currFrame.save("img/%s.gif" % frameNum, "GIF")
+    currFrame.convert("RGB").save("swap/%s.jpg" % frameNum)
     frameNum += 1
     try:
         currFrame.seek(frameNum)
     except EOFError:
         break;
 
-# # faceswap all frames (adapted from faceswap.py)
-# swapped = []
-# for frame in frames:
-# 	im1, landmarks1 = read_im_and_landmarks(frame) # gif
-# 	M = transformation_from_points(landmarks1[ALIGN_POINTS],
-# 	                               landmarks2[ALIGN_POINTS])
-# 	warped_mask = warp_im(mask, M, im1.shape)
-# 	combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
-# 	                          axis=0)
+# faceswap all frames (adapted from faceswap.py)
+for frame in range(frameNum):
+	try:
+		im1, landmarks1 = read_im_and_landmarks("swap/%s.jpg" % frame)
+		M = transformation_from_points(landmarks1[ALIGN_POINTS],
+		                               landmarks2[ALIGN_POINTS])
+		warped_mask = warp_im(mask, M, im1.shape)
+		combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
+		                          axis=0)
 
-# 	warped_face = warp_im(face, M, im1.shape)
-# 	warped_corrected_face = correct_colours(im1, warped_face, landmarks1)
+		warped_face = warp_im(face, M, im1.shape)
+		warped_corrected_face = correct_colours(im1, warped_face, landmarks1)
 
-# 	output_im = im1 * (1.0 - combined_mask) + warped_corrected_face * combined_mask
+		output_im = im1 * (1.0 - combined_mask) + warped_corrected_face * combined_mask
 
-# 	swapped.append(output_im)
+		cv2.imwrite("swap/%s.jpg" % frame, output_im)
+	except:
+		pass
 
+frames = []
+for frame in range(frameNum):
+    frames.append(imageio.imread("swap/%s.jpg" % frame))
+imageio.mimsave("img/faceswap.gif", frames, fps=20)
+shutil.rmtree("swap");
