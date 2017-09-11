e86ef5d6f9b32213be219271744e89feb10ec34e
marsalad <marsalad@sas.upenn.edu>
2017-09-11 19:53:00 -0400

    changed spaces to tabs

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var gif = ""
 var htmls = ["","","","","",""]
 
 $(document).ready(function() {
-    $('#search-bar').keypress(function(e) {
-        if(e.keyCode == 13) {
-            search();
-        }
-    });
+	$('#search-bar').keypress(function(e) {
+		if(e.keyCode == 13) {
+			search();
+		}
+	});
 });
 
 function search() {
@@ -33,36 +33,36 @@ function selectGif(caller) {
 }
 
 function previewFile() {
-    // Where you will display your image
-    var preview = document.getElementById("preview");
-    // The button where the user chooses the local image to display
-    file = document.querySelector('input[type=file]').files[0];
-    // FileReader instance
-    var reader  = new FileReader();
+	// Where you will display your image
+	var preview = document.getElementById("preview");
+	// The button where the user chooses the local image to display
+	file = document.querySelector('input[type=file]').files[0];
+	// FileReader instance
+	var reader  = new FileReader();
 
-    // When the image is loaded we will set it as source of
-    // our img tag
-    reader.onloadend = function () {
-        preview.src = reader.result;
-    }
+	// When the image is loaded we will set it as source of
+	// our img tag
+	reader.onloadend = function () {
+		preview.src = reader.result;
+	}
 
-    if (file) {
-	    // Load image as a base64 encoded URI
-	    reader.readAsDataURL(file);
-    } else {
-        preview.src = "";
-    }
+	if (file) {
+		// Load image as a base64 encoded URI
+		reader.readAsDataURL(file);
+	} else {
+		preview.src = "";
+	}
 }
 
 function swapFaces() {
-    $.ajax({
-        type: "POST",
-        url: "/swapFaces",
-        contentType:"application/json; charset=utf-8",
-        data: JSON.stringify({ gif: gif, img: file.name }),
-        processData: false,
-        success: function(data) {
-        	window.open("img/" + data + ".gif", "_blank");
-        }
-    });
+	$.ajax({
+		type: "POST",
+		url: "/swapFaces",
+		contentType:"application/json; charset=utf-8",
+		data: JSON.stringify({ gif: gif, img: file.name }),
+		processData: false,
+		success: function(data) {
+			window.open("img/" + data + ".gif", "_blank");
+		}
+	});
 }
