59c931e3012f3fd959bca6d89c90a49c2b7f092c
Diana Marsala <marsalad@sas.upenn.edu>
2017-09-12 15:57:27 -0400

    altered for use on heroku

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
-from faceswap import *
-import os, requests, time
-from flask import Flask, send_from_directory, request
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
-@app.route("/swapFaces", methods=["GET", "POST"])
-def swapFaces():
-	response = request.get_json(cache=False)
-	gif_url = response['gif']
-	img_name = response['img']
-	with open('img/tmp.gif', 'wb') as f:
-		f.write(requests.get(gif_url).content)
-	uniqueId = str(int(time.time()))
-	os.system('python processGif.py %s %s %s' %("img/tmp.gif", "img/" + img_name, uniqueId))
-	return uniqueId
-
-@app.route('/<path:path>')
-def send_static(path):
-    return send_from_directory("",path)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
