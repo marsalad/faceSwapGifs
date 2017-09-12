fbdbf016fafa03026f78ee0c84379ad265a618dd
marsalad <marsalad@sas.upenn.edu>
2017-09-12 11:41:00 -0400

    commenting and cleaning

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-var file = ""
-var gif = ""
-var htmls = ["","","",""]
-var giphy_key = '{{ env('GIPHY_KEY') }}';
+var file = ""                            // url of uploaded file
+var gif = ""                             // url of selected GIF
+var htmls = ["","","",""]                // url of displayed GIFs
+var giphyKey = '{{ env('GIPHY_KEY') }}'; // API Key for access to GIPHY search
 
+// On load, display 4 trending GIFs
 $(document).ready(function() {
-	$("#search-bar").keypress(function(e) {
-		if(e.keyCode == 13) {
-			search();
-		}
-	});
 	var xhr = $.get("https://api.giphy.com/v1/gifs/trending?"
-		+ "api_key=" + giphy_key + "&limit=4",
+		+ "api_key=" + giphyKey + "&limit=4",
 		function(response) {
 			$(response.data).each(function(index,value) {
 				document.getElementById("search-results").children[index].innerHTML = 
@@ -21,15 +17,19 @@ $(document).ready(function() {
 		});
 });
 
-$("input[type='image']").click(function() {
-    $("input[id='my_file']").click();
+// Run search when user presses enter
+$("#search-bar").keypress(function(e) {
+	if(e.keyCode == 13) {
+		search();
+	}
 });
 
+// Pull top 4 GIFs based on search terms
 function search() {
 	text = document.getElementById("search-bar").value;
 	text = text.replace(/\s/g, "+");
 	var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" 
-		+ text + "&api_key=" + giphy_key + "&limit=4",
+		+ text + "&api_key=" + giphyKey + "&limit=4",
 		function(response) {
 			$(response.data).each(function(index,value) {
 				document.getElementById("search-results").children[index].innerHTML = 
@@ -40,33 +40,30 @@ function search() {
 		})
 }
 
+// User selects the GIF to faceswap
 function selectGif(caller) {
 	gif = htmls[caller.parentNode.id.substring(4,5)-1];
 	document.getElementById("input-gif").children[0].src = gif;
 }
 
+// Update preview of uploaded image
 function previewFile() {
-	// Where you will display your image
 	var preview = document.getElementById("preview");
-	// The button where the user chooses the local image to display
 	file = document.querySelector('input[type=file]').files[0];
-	// FileReader instance
-	var reader  = new FileReader();
-
-	// When the image is loaded we will set it as source of
-	// our img tag
+	var reader = new FileReader();
+	
 	reader.onloadend = function () {
 		preview.src = reader.result;
 	}
 
 	if (file) {
-		// Load image as a base64 encoded URI
 		reader.readAsDataURL(file);
 	} else {
 		preview.src = "";
 	}
 }
 
+// Interface with Python scripts to return faceswapped GIF
 function swapFaces() {
 	$.ajax({
 		type: "POST",
