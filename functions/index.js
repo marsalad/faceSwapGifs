525dbb9ee4876ea3a6c08f5324c4beef5b26478f
marsalad <buggymarsala@gmail.com>
2018-01-05 01:16:35 -0500

    working http request to cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const cors = require('cors')({ origin: true });
+
+exports.queryGiphy = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    var xhr = $.get('https://api.giphy.com/v1/gifs/' + req.query.query 
+      + 'api_key=' + functions.config().giphy.key + '&limit=' + req.query.num);
+    xhr.done(function(data) {
+      res.status(200).send({data.data});
+    });
+  });
+});
--- public/js/gifs.js
+++ public/js/gifs.js
@@ -1,41 +1,56 @@
+// initialize firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyDp-FT-Bufv0jKR1WiGqhCg-Jkdh7RVCMM",
+  authDomain: "faceswapgifs.firebaseapp.com",
+  databaseURL: "https://faceswapgifs.firebaseio.com",
+  projectId: "faceswapgifs",
+  storageBucket: "faceswapgifs.appspot.com",
+  messagingSenderId: "813682175895"
+});
+
 // On load, display 4 trending GIFs
 $(document).ready(function() {
-	getGifs("");
+  getGifs('');
 })
 
 // Run search when user presses enter
-$("#search-bar").keypress(function(e) {
-	if (e.keyCode == 13) {
-		search()
-	}
+$('#search-bar').keypress(function(e) {
+  if (e.keyCode == 13) {
+    search()
+  }
 })
 
 // update GIFs per user specified search terms, default is trending
 function getGifs(query) {
-	$.ajax({
-		type: "POST",
-		url: "/searchGifs",
-		contentType:"application/json; charset=utf-8",
-		data: JSON.stringify({ query: query }),
-		processData: false,
-		success: function(data) {
-			var htmls = JSON.parse(data)
-			for (i = 0; i < htmls.length; i++) {
-				document.getElementById(
-					"search-results").children[i].children[0].src = htmls[i];
-			}
-		}
-	});
+  if(query) {
+    requestSnippet = 'search?q=' + query + '&'
+  } else {
+    requestSnippet = 'trending?'
+  }
+
+  $.ajax({
+    url: 'https://us-central1-faceswapgifs.cloudfunctions.net/queryGiphy',
+    type: 'GET',
+    contentType: 'application/json',
+    data: {
+      query: requestSnippet,
+      num: 4
+    },
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
 }
 
 // Pull top 4 GIFs based on search terms
 function search() {
-	text = document.getElementById("search-bar").value;
-	text = text.replace(/\s/g, "+");
-	getGifs(text);
+  getGifs(document.getElementById('search-bar').value.replace(/\s/g, '+'));
 }
 
 // User selects the GIF to faceswap
 function selectGif(caller) {
-	document.getElementById("input-gif").children[0].src = caller.src;
+  document.getElementById('input-gif').children[0].src = caller.src;
 }
